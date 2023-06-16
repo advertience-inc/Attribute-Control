1fdafa57cb8bee2f51578179c463700b66517d6a
ethanglide <ecook2489@gmail.com>
2023-06-16 14:39:35 -0400

    Update attributeControl.js
    Optimize onload function to not create entire new object, just add the event to the existing object
    
    Add onerror function so the adv_pageview event gets pushed regardless of if the request succeeds or not

--- attributeControl.js
+++ attributeControl.js
@@ -1,18 +1,22 @@
+// Send GET request to advertience API, then push returned data to dataLayer
 window.attributeControl = (clientTab) => {
-    // Send GET request to advertience API, then push returned data to dataLayer
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        const data = JSON.parse(xhttp.responseText);
-      
-        window.dataLayer = window.dataLayer || [];
-        dataLayer.push({
-          event: 'adv_pageview',
-          source: data.source,
-          medium: data.medium,
-          campaign: data.campaign
-      });
-    }
+  const xhttp = new XMLHttpRequest();
 
-    xhttp.open('GET', 'https://advertience-api.advertience.com/v1/attribute/' + clientTab + '/' + window.location.search);
-    xhttp.send();
+  xhttp.onload = () => {
+    const data = JSON.parse(xhttp.responseText);
+    data.event = 'adv_pageview';
+  
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push(data);
+  };
+
+  xhttp.onerror = () => {
+    console.log('Error while sending GET request to Advertience API');
+
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({event: 'adv_pageview'});
+  };
+  
+  xhttp.open('GET', 'https://advertience-api.advertience.com/v1/attribute/' + clientTab + '/' + window.location.search);
+  xhttp.send();
 }
